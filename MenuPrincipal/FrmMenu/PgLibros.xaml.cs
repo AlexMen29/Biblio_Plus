4e7d3c5d0268657c064756f5f0e31de978f0c204
alexm <alexm@DESKTOP-03IRMT5>
2024-09-17 17:19:02 -0600

    Actualizaciones para Mostrar libros, base establecida para implementar vista de libros por genero

--- MenuPrincipal/FrmMenu/PgLibros.xaml.cs
+++ MenuPrincipal/FrmMenu/PgLibros.xaml.cs
@@ -26,20 +26,28 @@ namespace MenuPrincipal.FrmMenu
         public PgLibros()
         {
             InitializeComponent();
-            CargarDatos();
-            ConfigurarSlider();
+            CargarTodosLosElementos();
+
+            //ConfigurarSlider();
         }
 
 
         SqlConnection conDB = new SqlConnection(MenuPrincipal.Properties.Settings.Default.conexionDB);
 
-        private void CargarDatos()
-        {
 
+        //Funcion creada para llamar solo una vez para cargar todos los elementos
+        private void CargarTodosLosElementos()
+        {
+            CargarElementosPorGenero("SELECT TOP 3* FROM prueba ORDER BY id DESC;", contentGrid);
+            CargarElementosPorGenero("SELECT *FROM prueba WHERE genero='Drama'",contenGridDrama);
+        }
+
+        //funcion que carga elementos segun genero
+        private void CargarElementosPorGenero(string query,ItemsControl contenGrid)
+        {
             try
             {
-                conDB.Open();
-                string query = "SELECT * FROM prueba";
+                conDB.Open();           
                 SqlCommand command = new SqlCommand(query, conDB);
 
                 SqlDataAdapter adapter = new SqlDataAdapter(command);
@@ -61,39 +69,18 @@ namespace MenuPrincipal.FrmMenu
                 }
 
                 // Asignar los datos al ItemsControl
-                contentGrid.ItemsSource = dataTable.DefaultView;
+                contenGrid.ItemsSource = dataTable.DefaultView;
+
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"Error al conectar a la base de datos: {ex.Message}");
             }
-            finally { 
+            finally
+            {
                 conDB.Close();
             }
         }
-        private void ConfigurarSlider()
-        {
-            if (dataTable != null)
-            {
-                // Configurar el slider según la cantidad de imágenes cargadas
-                ImageSlider.Maximum = dataTable.Rows.Count - 1;
-            }
-        }
-
-        // Evento que cambia la imagen mostrada en el slider
-        private void ImageSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            if (dataTable != null)
-            {
-                int index = (int)ImageSlider.Value;
-                DataRow row = dataTable.Rows[index];
-
-                // Mostrar la imagen seleccionada en el ImageDisplay
-                ImageDisplay.Source = (BitmapImage)row["ImageData"];
-            }
-        }
-
-        // Método para convertir un array de bytes a BitmapImage
         private BitmapImage ConvertirABitmapImage(byte[] imageBytes)
         {
             using (var ms = new System.IO.MemoryStream(imageBytes))
