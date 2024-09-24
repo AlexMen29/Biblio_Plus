42a57fd78cf09583b06fc47a52c0c1a5f450c041
Daniel Rivas <danielrivaa4@gmail.com>
2024-09-24 16:32:04 -0600

    Ya funciona para filtrar a los usuarios por carnet, nombre, carrera y correo

---
+++ MenuPrincipal/PageUsuarios/Services/DatoUsuario.cs
@@ -0,0 +1,63 @@
+﻿using MenuPrincipal.PageUsuarios.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace MenuPrincipal.PageUsuarios.Services
+{
+    public class DatoUsuario
+    {
+        public DatoUsuario()
+        {
+
+        }
+
+
+        //METODO PARA CARGAR DATOS AL DATAGRID
+        public static List<UsuariosModel> MuestraUsuario()
+        {
+            List<UsuariosModel> lsUsuarios = new List<UsuariosModel>();
+
+            try
+            {
+                using (var conn = new SqlConnection(Properties.Settings.Default.conexionDB))
+                {
+                    conn.Open();
+                    using (var command = conn.CreateCommand())
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = "SPMOSTRARUSUARIO";
+                        using (DbDataReader dr = command.ExecuteReader())
+                        {
+                            //recorrer dataGrid
+                            while (dr.Read())
+                            {
+                                UsuariosModel user = new UsuariosModel();
+                                user.UsuarioId = int.Parse(dr["USUARIOID"].ToString());
+                                user.NombreCompleto = dr["NOMBRECOMPLETO"].ToString();
+                                user.Correo = dr["CORREO"].ToString();
+                                user.Clave = dr["CLAVE"].ToString();
+
+                                //Agregar a la lista inicial
+                                lsUsuarios.Add(user);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Ocurrio un error al intentar mostrar el registro: " + e.Message, "Validacion",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            return lsUsuarios;
+        }
+    }
+}
--- MenuPrincipal/PageUsuarios/Usuarios.xaml.cs
+++ MenuPrincipal/PageUsuarios/Usuarios.xaml.cs
@@ -10,8 +10,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace MenuPrincipal.PageUsuarios
 {
@@ -20,9 +21,95 @@ namespace MenuPrincipal.PageUsuarios
     /// </summary>
     public partial class Usuarios : Page
     {
+        private string connectionString = "Data Source=LAPTOP-1QS2UH3M\\SQLEXPRESS;Initial Catalog=Prueba;Integrated Security=True";
         public Usuarios()
         {
             InitializeComponent();
+        }
+        //CONEXION  A LA BASE DE DATOS 
+        SqlConnection condb = new SqlConnection(Properties.Settings.Default.conexionDB);
+
+        //Variable consulata sql 
+        String consultaSQL = null;
+
+        void MostrarUsuario()
+        {
+            //consulta sql 
+            consultaSQL = null;
+            consultaSQL = "SELECT Carnet,NombreCompleto,Carrera,Correo,Telefono FROM USUARIOS";
+            condb.Open();
+            //creando elemento sqladapter
+            SqlDataAdapter da = new SqlDataAdapter(consultaSQL, condb);
+            // crear data table
+            DataTable dt = new DataTable();
+            // llenar table 
+            da.Fill(dt);
+            // proceder a llenado de datagrid
+            dataGridUsuarios.ItemsSource = dt.DefaultView;
+            //cerrar 
+            condb.Close();
+
+        }
+
+        private void BuscarUsuarios()
+        {
+            string carnet = txtCarnet.Text;
+            string nombreCompleto = txtNombre.Text;
+            string carrera = txtCarrera.Text;
+            string correo = txtCorreo.Text;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = con;
+
+                    // Consulta SQL dinámica
+                    string query = "SELECT * FROM USUARIOS WHERE 1=1"; // 1=1 para no tener que revisar si es el primer filtro
+
+                    if (!string.IsNullOrEmpty(carnet))
+                    {
+                        query += " AND Carnet LIKE @Carnet";
+                        cmd.Parameters.AddWithValue("@Carnet", "%" + carnet + "%");
+                    }
+                    if (!string.IsNullOrEmpty(nombreCompleto))
+                    {
+                        query += " AND NombreCompleto LIKE @NombreCompleto";
+                        cmd.Parameters.AddWithValue("@NombreCompleto", "%" + nombreCompleto + "%");
+                    }
+                    if (!string.IsNullOrEmpty(carrera))
+                    {
+                        query += " AND Carrera LIKE @Carrera";
+                        cmd.Parameters.AddWithValue("@Carrera", "%" + carrera + "%");
+                    }
+                    if (!string.IsNullOrEmpty(correo))
+                    {
+                        query += " AND Correo LIKE @Correo";
+                        cmd.Parameters.AddWithValue("@Correo", "%" + correo + "%");
+                    }
+
+                    cmd.CommandText = query;
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    dataGridUsuarios.ItemsSource = dt.DefaultView; // Mostrar resultados en el DataGrid
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al buscar usuarios: " + ex.Message);
+                }
+            }
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MostrarUsuario();
+        }
+
+        private void btnFiltrar_Click(object sender, RoutedEventArgs e)
+        {
+            BuscarUsuarios();
         }
     }
 }
